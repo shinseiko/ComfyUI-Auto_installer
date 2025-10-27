514f43a266bb749d1016b0a65752cf06b1a9e6bd
Samuel Moreno <contact@smoreno.me>
2025-10-27 10:06:34 +0100

    change links

--- scripts/Bootstrap-Downloader.ps1
+++ scripts/Bootstrap-Downloader.ps1
@@ -3,7 +3,7 @@ param(
 )
 
 # Set the base URL for the GitHub repository's raw content
-$baseUrl = "https://github.com/UmeAiRT/ComfyUI-Auto_installer/raw/main/"
+$baseUrl = "https://github.com/UmeAiRT/ComfyUI-Auto_installer/raw/feature-conda-integration/"
 
 # Define the list of files to download
 $filesToDownload = @(
